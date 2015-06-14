fca4229a30cfe38d64792ec1a09af8a00206c46d
Matt Westcott <m.westcott@gmail.com>
2015-06-14 22:48:09 +0100

    Move test_toolset.py to main folder and update it

--- pyeuler/test_toolset.py
+++ pyeuler/test_toolset.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import unittest
 
-from pyeuler.toolset import *
+from toolset import *
 
 class TestToolset(unittest.TestCase):
     def test_take(self):
@@ -10,11 +10,9 @@ class TestToolset(unittest.TestCase):
         self.assertEqual(list(take(0, [1,2,3])), [])
         self.assertEqual(list(take(100, [1,2,3])), [1,2,3])
 
-    def test_index(self):
-        self.assertEqual(index(0, [1,2,3,4]), 1)
-        self.assertEqual(index(3, [1,2,3,4]), 4)
-        self.assertRaises(StopIteration, index, 0, [])
-        self.assertRaises(StopIteration, index, 10, [1,2,3])
+    def test_nth(self):
+        self.assertEqual(nth([1,2,3,4], 0), 1)
+        self.assertEqual(nth([1,2,3,4], 3), 4)
 
     def test_first(self):
         self.assertEqual(first(iter([1,2,3,4])), 1)
@@ -45,9 +43,9 @@ class TestToolset(unittest.TestCase):
         self.assertEqual(product([2, 3]), 6)
 
     def test_flatten(self):
-        self.assertEqual(flatten([]), [])
-        self.assertEqual(flatten([[1,2,3], [4,5,6]]), [1,2,3,4,5,6])
-        self.assertEqual(flatten([[1,2,3], [4,[5,6]]]), [1,2,3,4,[5,6]])
+        self.assertEqual(list(flatten([])), [])
+        self.assertEqual(list(flatten([[1,2,3], [4,5,6]])), [1,2,3,4,5,6])
+        self.assertEqual(list(flatten([[1,2,3], [4,[5,6]]])), [1,2,3,4,[5,6]])
 
     def test_compact(self):
         self.assertEqual(list(compact([0, 1, "", None, [], (), "hello"])), [1, "hello"])
