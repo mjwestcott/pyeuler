01392cd15c4dcb44493536fbb59b5d716ec18d0c
Matt Westcott <m.westcott@gmail.com>
2015-06-14 20:33:21 +0100

    Add solution to problem 51

--- pyeuler/problems.py
+++ pyeuler/problems.py
@@ -477,3 +477,35 @@ def problem50():
             return get_max_length(primes, n+1, max_length, acc)
     primes = persistent(get_primes())
     return get_max_length(primes, 0)
+
+def problem51():
+    """By replacing the 1st digit of the 2-digit number *3, it turns out that six of
+    the nine possible values: 13, 23, 43, 53, 73, and 83, are all prime.
+
+    By replacing the 3rd and 4th digits of 56**3 with the same digit, this
+    5-digit number is the first example having seven primes among the ten
+    generated numbers, yielding the family: 56003, 56113, 56333, 56443, 56663,
+    56773, and 56993. Consequently 56003, being the first member of this family,
+    is the smallest prime with this property.
+
+    Find the smallest prime which, by replacing part of the number (not
+    necessarily adjacent digits) with the same digit, is part of an eight prime
+    value family."""
+    def replace_digits(num, mask, val):
+        """Replace digits in num with val at indicies specified by the mask.
+        If result leads with a zero, return a sentinel value (-1)."""
+        # replace_digits(3537, [1, 1, 0, 0], 9) --> 9937
+        apply_mask = lambda x, y: val if y else x
+        digits = list(map(apply_mask, digits_from_num_fast(num), mask))
+        return int(''.join(map(str, digits))) if digits[0] != 0 else -1
+    # For each prime above 56995, for all possible binary masks
+    # representing ways to replace digits in that number, yield
+    # the corresponding family of ten digits
+    families = ([replace_digits(n, mask, val) for val in range(10)]
+                for n in get_primes(start=56995)
+                for mask in binary_nums(ilen(digits_from_num_fast(n))))
+    # Find the next solution, where solution is the first prime
+    # member of the family for which eight members are prime.
+    return next(first_true(family, pred=is_prime)
+                for family in families
+                if quantify(family, pred=is_prime) == 8)
