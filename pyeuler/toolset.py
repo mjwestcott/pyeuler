0deb14cd7c4e89cde65430eb9e57e87cb04e8856
Matt Westcott <m.westcott@gmail.com>
2015-10-16 20:09:27 +0100

    Add solution to problem 69
    Also, use integer division in prime_factors() in toolset.py as
    intended from python 2.7
    
    The solution is not as functional in style as I would like.

--- pyeuler/toolset.py
+++ pyeuler/toolset.py
@@ -238,7 +238,7 @@ def prime_factors(num, start=2):
     """Return all prime factors (ordered) of num in a list"""
     candidates = range(start, int(sqrt(num)) + 1)
     factor = next((x for x in candidates if (num % x == 0)), None)
-    return ([factor] + prime_factors(num / factor, factor) if factor else [num])
+    return ([factor] + prime_factors(num // factor, factor) if factor else [num])
 
 def factorize(num):
     """Factorize a number returning occurrences of its prime factors"""
