453815b06239fe008339768e5777d8dddbbba935
Matt Westcott <m.westcott@gmail.com>
2015-10-30 16:18:33 +0000

    Add solution to problem 80

--- pyeuler/problems51_100.py
+++ pyeuler/problems51_100.py
@@ -836,3 +836,23 @@ def problem79():
                    for y in after[x])
     solution = solve()
     return num_from_digits(solution)
+
+def problem80():
+    """It is well known that if the square root of a natural number is not an
+    integer, then it is irrational. The decimal expansion of such square roots
+    is infinite without any repeating pattern at all.
+
+    The square root of two is 1.41421356237309504880..., and the digital sum of the
+    first one hundred decimal digits is 475.
+
+    For the first one hundred natural numbers, find the total of the digital sums
+    of the first one hundred decimal digits for all the irrational square roots."""
+    # Set the precision of decimal arithmetic operations to slightly above 100
+    # to ensure that the first 100 digits are correct.
+    import decimal; decimal.getcontext().prec = 102
+    def square_root_digits(n):
+        "Find the first one hundred decimal digits of the square root of n"
+        return decimal.Decimal(n).sqrt().as_tuple().digits[:-2] # Cut off the last 2 of the 102 digits.
+    def has_irrational_sqrt(n):
+        return sqrt(n).is_integer() == False
+    return sum(sum(square_root_digits(n)) for n in range(1, 100) if has_irrational_sqrt(n))
